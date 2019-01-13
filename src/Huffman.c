2aaffb0d5aeea4b99998c844520de3d9ac461bbb
ubap <jaktrzeb@gmail.com>
2019-01-13 17:59:52 +0100

    Binary codes, start writing to file

--- src/Huffman.c
+++ src/Huffman.c
@@ -15,10 +15,12 @@
 #include <time.h>
 #include <math.h>
 
-#define MAX_CODE_LENGTH 128
+#define MAX_CODE_LENGTH 32
 #define BITS_8  2048
 
 int get_maximum_count_index(int* table, int N);
+void reverse_string(char *str);
+double calculate_codes();
 
 int byte_count[BITS_8];
 
@@ -31,7 +33,8 @@ typedef struct symbol
     uint8_t is_primitive;
 	uint8_t primitive_symbol; //
 	uint8_t code_length;
-	char primitive_code[MAX_CODE_LENGTH];
+    uint32_t primitive_code;
+	char primitive_code_string[MAX_CODE_LENGTH];
 };
 
 struct symbol symbols_table[BITS_8];
@@ -56,39 +59,110 @@ int compare_symbol_index_p (const void * a, const void * b)
     else return 1;
 }
 
-int main(void) {
+int main(int argc, char **argv) {
     clock_t start = clock();
 	// printf("%i", sizeof(character_count)); zwr�ci sizeof(int) * BITS_8
 	//memset(character_count, 0, sizeof(character_count));
-
-    FILE *f = fopen("notatki.txt", "rb");    // otwiera plik do odczytu, tryb binarny
-
-    if (f == NULL) //  (musi istniec)
+    FILE * f_in;
+    if(argc > 1)
     {
-        perror("Nie udalo sie otworzyc pliku notatki.txt");
+        f_in = fopen(argv[1], "rb");
+    } else 
+    {
+        f_in = fopen("notatki.txt", "rb");    // otwiera plik do odczytu, tryb binarny
+    }
+
+    if (f_in == NULL) //  (musi istniec)
+    {
+        perror("Nie udalo sie otworzyc pliku");
         return 1;
     }
-    puts("Plik otwarty pomyslnie!");
 
+    printf("Plik otwarty pomyslnie!");
 
     uint8_t data_byte = 0;
-    while (fread(&data_byte, 1, 1, f))
+    while (fread(&data_byte, 1, 1, f_in))
     {
     	byte_count[data_byte]++;
-    }
+    }   //count number of occurences of all characters
 
-
-
-    fseek(f, 0, 2); // (file, offset, mode) / mode=2 -> pos = eof - offset
-    int x = ftell(f);
+    fseek(f_in, 0, 2); // (file, offset, mode) / mode=2 -> pos = eof - offset
+    int x = ftell(f_in); //find size of file
     printf("Rozmiar pliku %i Bytes.\n", x);
-    fseek(f,0,0); // mode = 0 -> pos = offset
-    int index = get_maximum_count_index(byte_count, BITS_8);
-    printf("Ilosc: a: %i, Spacji: %i, z: %i, G: %i\n", byte_count[(int)'a'], byte_count[(int)' '], byte_count[(int)'z'], byte_count[(int)'G']);
+
+    fseek(f_in,0,0); // mode = 0 -> pos = offset
+    int index = get_maximum_count_index(byte_count, BITS_8); //find maximum count
     printf("Najwiecej jest: [%c/%i]\n", (char)index, byte_count[index] );
 
-    fclose(f);
+    
 
+    /*Calculate Huffman codes of characters*/
+    /*uses global variables:
+        struct symbol symbols_table[BITS_8];
+        int symbols_count;
+        int byte_count[BITS_8];
+    */
+    double Lsr = calculate_codes();
+
+    //use table: symbols_table, and var symbols_count to translate file and make a new file
+    FILE *f_out;
+    if(argc > 2)
+    {
+        f_out = fopen(argv[2], "wb");
+    } else 
+    {
+        f_out = fopen("out.txt", "wb");    // otwiera plik do odczytu, tryb binarny
+    }
+
+    if (f_out == NULL) //  (musi istniec)
+    {
+        perror("Nie udalo sie utworzyc nowego pliku");
+        return 1;
+    }
+
+    printf("Plik otwarty pomyslnie!");
+
+    data_byte = 0;
+    uint32_t read_size = 0;
+    uint32_t write_buffer = 0;
+    uint8_t buffer_i = 31;
+    uint8_t symbol_index = 0;
+    struct symbol *s;
+    
+
+    //laduj bajty z f_in, translatuj i wpisuj do f_out
+    while (fread(&data_byte, 1, 1, f_in))
+    {
+        symbol_index = get_symbol_index(data_byte); //TODO
+        s = &symbols_table[symbol_index];
+    	write_buffer += (s->primitive_code << (buffer_i - s->code_length));
+        //bedzie zapisywac 4bajty do pliku gdy zapelni sie 4 bajtowy bufor
+        //byte_count[data_byte]++;
+        read_size = fread(&data_byte,1,1,f_out);
+
+    }   //count number of occurences of all characters
+
+    fseek(f_out, 0, 2); // (file, offset, mode) / mode=2 -> pos = eof - offset
+    x = ftell(f_out); //find size of file
+    printf("Rozmiar pliku wyjsciowego %i Bytes.\n", x);
+
+    fseek(f_out,0,0); // mode = 0 -> pos = offset
+
+    fclose(f_out);
+    fclose(f_in);
+
+    printf("Lsr: %f\n", Lsr);
+    clock_t end = clock();
+
+    double cpu_time_used = ((double) (end - start)) / (double)CLOCKS_PER_SEC;
+    printf("for loop took %f seconds to execute \n", cpu_time_used);
+
+	return EXIT_SUCCESS;
+}
+
+double calculate_codes()
+{
+    /*find sum of all character occurences*/
     uint64_t sum = 0;
     for(int i = 0; i < BITS_8; i++)
     {
@@ -99,17 +173,20 @@ int main(void) {
     printf("sum: %u\n", sum);
 
     double entropy = 0;
-    double p = 0;
-    symbols_count = 0;
+    double p = 0; //probability
+    symbols_count = 0; //how much various symbols
 
-    int symbols_index_table[BITS_8];
+    /*table with indexes of symbols in symbols_table, used for sorting and joining symbols
+    for Huffman algorithm*/
+    int symbols_index_table[BITS_8]; 
 
+    /*Find symbols probability and symbols count with nonzero probability*/
     for(int i = 0; i < BITS_8; i++)
     {
     	if(byte_count[i] != 0)
 		{
 			p =  (double)byte_count[i]/(double)sum;
-		    printf("p: %c, %f\n", (char)i, p);
+		    //printf("p: %c, %f\n", (char)i, p);
 			entropy = entropy + log2(p)*p;
 
             symbols_table[symbols_count].is_primitive = 1;
@@ -149,22 +226,21 @@ int main(void) {
         symbols_new_index++;
     }
 
-    for(int i = 0; i < symbols_new_index; i++)
+    /*for(int i = 0; i < symbols_new_index; i++)
     {
         printf("p: %f\n", symbols_table[i].p);
-    }
+    }*/
     // KODOWANIE SYMBOLI: END
 
     int child_i;
-    struct symbol *s, *parent;
-    int L = 0;
+    struct symbol *primitive, *s, *parent;
     double Lsr = 0;
-    double probal = 0;
     for(int i = 0; i < symbols_count; i++)
     {
         s = &symbols_table[i]; 
+        primitive = s;
         printf("%c[%f]: ", s->primitive_symbol, s->p);
-        probal = s->p;
+        p = s->p;
         child_i = i;
         while(s->parent_i != 0)
         {
@@ -172,30 +248,24 @@ int main(void) {
             
             if(parent->upper_child_i == child_i)
             {
-                child_i = s->parent_i;
-                printf("1");
-                L++;
+                primitive->primitive_code_string[primitive->code_length] = '1';
+                primitive->primitive_code+= (1 << primitive->code_length);
             }
             else if(parent->lower_child_i == child_i)
             {
-                child_i = s->parent_i;
-                printf("0");
-                L++;
+                primitive->primitive_code_string[primitive->code_length] = '0';
             }
+
+            child_i = s->parent_i;
+            primitive->code_length++;
             s = parent;
         }
-        printf("\n");
-        Lsr=Lsr+L*probal;
-        L = 0;
+        reverse_string(primitive->primitive_code_string);
+        printf("%s\n",primitive->primitive_code_string);
+        Lsr=Lsr+primitive->code_length*p;
     }
 
-    printf("Lsr: %f\n", Lsr);
-    clock_t end = clock();
-
-    double cpu_time_used = ((double) (end - start)) / (double)CLOCKS_PER_SEC;
-    printf("for loop took %f seconds to execute \n", cpu_time_used);
-
-	return EXIT_SUCCESS;
+    return Lsr;
 }
 
 int get_maximum_count_index(int* table, int N)
@@ -211,4 +281,37 @@ int get_maximum_count_index(int* table, int N)
 		}
 	}
 	return index;
+}
+
+void reverse_string(char *str)
+{
+    /* skip null */
+    if (str == 0)
+    {
+        return;
+    }
+
+    /* skip empty string */
+    if (*str == 0)
+    {
+        return;
+    }
+
+    /* get range */
+    char *start = str;
+    char *end = start + strlen(str) - 1; /* -1 for \0 */
+    char temp;
+
+    /* reverse */
+    while (end > start)
+    {
+        /* swap */
+        temp = *start;
+        *start = *end;
+        *end = temp;
+
+        /* move */
+        ++start;
+        --end;
+    }
 }
